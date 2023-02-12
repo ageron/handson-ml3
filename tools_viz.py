7d74cc02dfef908ae987696cc5204ea5278a9301
Tanay Sood <tanay.sd@gmail.com>
2023-02-11 20:34:37 -0800

    commit 2.11 II

---
+++ tools_viz.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+def set_viz_properties():
+    plt.rc('font', size=14)
+    plt.rc('axes', labelsize=14, titlesize=14)
+    plt.rc('legend', fontsize=14)
+    plt.rc('xtick', labelsize=10)
+    plt.rc('ytick', labelsize=10)
+    
+    return None
+
+def save_fig(IMAGES_PATH, fig_id, tight_layout=True, fig_extension="png", resolution=300):
+    path = IMAGES_PATH / f"{fig_id}.{fig_extension}"
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(path, format=fig_extension, dpi=resolution)
