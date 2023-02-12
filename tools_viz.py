e30bd01e8f23006a0e57fee248248cf72be5c1ec
Tanay Sood <tanay.sd@gmail.com>
2023-02-11 22:36:14 -0800

    commit 2.11 III

--- tools_viz.py
+++ tools_viz.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 def set_viz_properties():
     plt.rc('font', size=14)
@@ -14,3 +15,29 @@ def save_fig(IMAGES_PATH, fig_id, tight_layout=True, fig_extension="png", resolu
     if tight_layout:
         plt.tight_layout()
     plt.savefig(path, format=fig_extension, dpi=resolution)
+
+def plot_decision_boundary(clf, X, y, axes, cmap):
+    x1, x2 = np.meshgrid(np.linspace(axes[0], axes[1], 100),
+                         np.linspace(axes[2], axes[3], 100))
+    X_new = np.c_[x1.ravel(), x2.ravel()]
+    y_pred = clf.predict(X_new).reshape(x1.shape)
+    
+    plt.contourf(x1, x2, y_pred, alpha=0.3, cmap=cmap)
+    plt.contour(x1, x2, y_pred, cmap="Greys", alpha=0.8)
+    colors = {"Wistia": ["#78785c", "#c47b27"], "Pastel1": ["red", "blue"]}
+    markers = ("o", "^")
+    for idx in (0, 1):
+        plt.plot(X[:, 0][y == idx], X[:, 1][y == idx],
+                 color=colors[cmap][idx], marker=markers[idx], linestyle="none")
+    plt.axis(axes)
+    plt.xlabel(r"$x_1$")
+    plt.ylabel(r"$x_2$", rotation=0)
+
+def plot_regression_predictions(tree_reg, X, y, axes=[-0.5, 0.5, -0.05, 0.25]):
+    x1 = np.linspace(axes[0], axes[1], 500).reshape(-1, 1)
+    y_pred = tree_reg.predict(x1)
+    plt.axis(axes)
+    plt.xlabel("$x_1$")
+    plt.plot(X, y, "b.")
+    plt.plot(x1, y_pred, "r.-", linewidth=2, label=r"$\hat{y}$")
+    
