5b2fc4c282b474d78378c58bf84b64bb047acd0f
Mohamed Kandil <medokandeal7@gmail.com>
2025-12-24 21:31:12 +0200

    feat: Add scripts for environment setup, launch, and verification

---
+++ start_jupyterlab.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /d "k:\learning\technical\courses\Hands-On Machine Learning with Scikit-Learn, Keras, and TensorFlow\handson-ml3"
+call "%USERPROFILE%\miniconda3\Scripts\activate.bat" homl3
+start "" jupyter lab
